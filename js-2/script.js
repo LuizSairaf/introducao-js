14c6ad52304a28a40a9b6862e2f036143f2beddd
LuizSairaf <luizoprogramador@gmail.com>
2022-05-29 11:46:25 -0300

    adicionando informacoes

---
+++ js-2/script.js
@@ -0,0 +1,75 @@
+//----tipos primitivos----
+
+//boolean
+/*var vouf = false;
+console.log(vouf);
+console.log(typeof(vouf));//typeof = descobrir o tipo da variável.
+//number
+var numeroqualquer = 1;
+console.log(typeof(numeroqualquer));
+//string
+var nome = 'luiz';
+console.log(typeof(nome)); */
+
+//---- declarando variável----
+/*var variavel = 'luiz';
+console.log(typeof(variavel));
+let variavel2 = 'luiz';
+console.log(typeof(variavel2));
+const  constante = 'luiz'; // não se pode alterar o seu valor uma vez já declarada
+console.log(typeof(constante));*/
+
+//----escopo----
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escolpoLocalInterno = 'local';
+    console.log(escolpoLocalInterno);
+}
+
+escopoLocal();//chamando função */
+
+//atribuição
+/*var atribuicao = 'luiz';
+//comparacao
+var comparacao = '0' == 0;
+console.log(comparacao);
+//comparacao idêntica
+var comparacaoidentica = '0' === 0;
+console.log(comparacaoidentica);*/
+
+//divisão real
+/*var divisaoreal = 6 / 2;
+console.log(divisaoreal);
+//divisão inteira
+var divisaointeira = 5 % 2;
+console.log(divisaointeira);
+//potenciação
+var potenciacao = 2 ** 10;
+console.log(potenciacao);*/
+
+//----operadores relacionais----
+//maior que
+/*var maiorque = 5 > 2;
+console.log(maiorque);
+//menor que
+var menorque = 5 < 2;
+console.log(menorque);
+//maior ou igual a
+var maiorouigual = 5 >= 2;
+console.log(maiorouigual);
+//menor ou igual a
+var menorouigual = 5 <= 2;
+console.log(menorouigual);*/
+
+//operadores lógicos
+//e - && - considera todos os valores sejam true.
+/*var e = true && false;
+console.log(e);
+//ou - || -considera que qualquer valor seja true.
+var ou = false|| false;
+console.log(ou);
+//! - não - inverte o valor de true para false ou vice-versa.
+var nao = !true;
+console.log(nao);*/
